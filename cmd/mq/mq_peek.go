a31b296d3c3cf989f8bfe58e0bfcd1c4b7f3b4e8
Grigorij Eleskin <puri.world@yandex.ru>
2016-08-29 21:15:00 +0400

    Finish mq_peek, mq_pop commands

--- cmd/mq/mq_peek.go
+++ cmd/mq/mq_peek.go
@@ -1,27 +1,64 @@
 package mq
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/iron-io/iron_go3/config"
+	"github.com/iron-io/iron_go3/mq"
+	"github.com/iron-io/ironcli/common"
 	"github.com/urfave/cli"
 )
 
 type MqPeek struct {
+	number int
+
 	cli.Command
 }
 
 func NewMqPeek(settings *config.Settings) *MqPeek {
-	mqPeek := &MqPeek{
-		Command: cli.Command{
-			Name:      "peek",
-			Usage:     "do the doo",
-			UsageText: "doo - does the dooing",
-			ArgsUsage: "[image] [args]",
-			Action: func(c *cli.Context) error {
-				fmt.Println("added task: test ", c.Args().First())
-				return nil
+	mqPeek := &MqPeek{}
+
+	mqPeek.Command = cli.Command{
+		Name:      "peek",
+		Usage:     "peek messages by amount of queue",
+		ArgsUsage: "[QUEUE_NAME] [args]",
+		Flags: []cli.Flag{
+			cli.IntFlag{
+				Name:        "number, n",
+				Usage:       "number usage",
+				Destination: &mqPeek.number,
 			},
+		},
+		Action: func(c *cli.Context) error {
+			if c.Args().First() == "" {
+				return errors.New(`peek requires one arg`)
+			}
+
+			q := mq.ConfigNew(c.Args().First(), settings)
+
+			msgs, err := q.PeekN(mqPeek.number)
+			if err != nil {
+				return err
+			}
+
+			if len(msgs) < 1 {
+				return errors.New("Queue is empty.")
+			}
+
+			if !common.IsPipedOut() {
+				plural := ""
+				if mqPeek.number > 1 {
+					plural = "s"
+				}
+
+				fmt.Println(common.LINES, "Message", plural, " successfully peeked")
+				fmt.Println()
+				fmt.Println("-------- ID ------ | Body")
+			}
+			common.PrintMessages(msgs)
+
+			return nil
 		},
 	}
 
--- cmd/mq/mq_pop.go
+++ cmd/mq/mq_pop.go
@@ -1,27 +1,97 @@
 package mq
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"os"
 
 	"github.com/iron-io/iron_go3/config"
+	"github.com/iron-io/iron_go3/mq"
+	"github.com/iron-io/ironcli/common"
 	"github.com/urfave/cli"
 )
 
 type MqPop struct {
+	number     int
+	outputfile string
+	file       *os.File
+
 	cli.Command
 }
 
 func NewMqPop(settings *config.Settings) *MqPop {
-	mqPop := &MqPop{
-		Command: cli.Command{
-			Name:      "pop",
-			Usage:     "do the doo",
-			UsageText: "doo - does the dooing",
-			ArgsUsage: "[image] [args]",
-			Action: func(c *cli.Context) error {
-				fmt.Println("added task: test ", c.Args().First())
-				return nil
+	mqPop := &MqPop{}
+
+	mqPop.Command = cli.Command{
+		Name:      "pop",
+		Usage:     "pop messages by amount of queue",
+		ArgsUsage: "[QUEUE_NAME] [args]",
+		Flags: []cli.Flag{
+			cli.IntFlag{
+				Name:        "number, n",
+				Usage:       "number usage",
+				Destination: &mqPop.number,
 			},
+			cli.StringFlag{
+				Name:        "output, o",
+				Usage:       "output usage",
+				Destination: &mqPop.outputfile,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			if c.Args().First() == "" {
+				return errors.New(`pop requires a queue name`)
+			}
+
+			if mqPop.outputfile != "" {
+				f, err := os.Create(mqPop.outputfile)
+				if err != nil {
+					return err
+				}
+
+				mqPop.file = f
+			}
+
+			q := mq.ConfigNew(c.Args().First(), settings)
+
+			messages, err := q.PopN(mqPop.number)
+			if err != nil {
+				return err
+			}
+
+			// If anything here fails, we still want to print out what was deleted before exiting
+			if mqPop.file != nil {
+				b, err := json.Marshal(messages)
+				if err != nil {
+					common.PrintMessages(messages)
+
+					return err
+				}
+
+				_, err = mqPop.file.Write(b)
+				if err != nil {
+					common.PrintMessages(messages)
+
+					return err
+				}
+			}
+
+			if common.IsPipedOut() {
+				common.PrintMessages(messages)
+			} else {
+				plural := ""
+				if mqPop.number > 1 {
+					plural = "s"
+				}
+
+				fmt.Println(common.LINES, "Message", plural, " successfully popped off ", q.Name)
+				fmt.Println()
+				fmt.Println("-------- ID ------ | Body")
+				common.PrintMessages(messages)
+			}
+
+			return nil
 		},
 	}
 
