5ae05a4edc99a0937bd5b2e48c664e277e26d0c8
Grigorij Eleskin <puri.world@yandex.ru>
2016-08-29 21:03:35 +0400

    Finish mq_info, mq_list commands

--- cmd/mq/mq_info.go
+++ cmd/mq/mq_info.go
@@ -1,27 +1,67 @@
 package mq
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/iron-io/iron_go3/config"
+	"github.com/iron-io/iron_go3/mq"
+	"github.com/iron-io/ironcli/common"
 	"github.com/urfave/cli"
 )
 
 type MqInfo struct {
+	subscriberList bool
+
 	cli.Command
 }
 
 func NewMqInfo(settings *config.Settings) *MqInfo {
-	mqInfo := &MqInfo{
-		Command: cli.Command{
-			Name:      "info",
-			Usage:     "do the doo",
-			UsageText: "doo - does the dooing",
-			ArgsUsage: "[image] [args]",
-			Action: func(c *cli.Context) error {
-				fmt.Println("added task: test ", c.Args().First())
-				return nil
+	mqInfo := &MqInfo{}
+	mqInfo.Command = cli.Command{
+		Name:      "info",
+		Usage:     "get info about queue",
+		ArgsUsage: "[QUEUE_NAME] [args]",
+		Flags: []cli.Flag{
+			cli.BoolFlag{
+				Name:        "subscriber-list",
+				Usage:       "subscriber-list usage",
+				Destination: &mqInfo.subscriberList,
 			},
+		},
+		Action: func(c *cli.Context) error {
+			if c.Args().First() == "" {
+				return errors.New(`info requires a queue name`)
+			}
+
+			q := mq.ConfigNew(c.Args().First(), settings)
+
+			info, err := q.Info()
+			if err != nil {
+				return err
+			}
+
+			fmt.Printf("%sName: %s\n", common.BLANKS, info.Name)
+			fmt.Printf("%sCurrent Size: %d\n", common.BLANKS, info.Size)
+			fmt.Printf("%sTotal messages: %d\n", common.BLANKS, info.TotalMessages)
+			fmt.Printf("%sMessage expiration: %d\n", common.BLANKS, info.MessageExpiration)
+			fmt.Printf("%sMessage timeout: %d\n", common.BLANKS, info.MessageTimeout)
+
+			if info.Push != nil {
+				fmt.Printf("%sType: %s\n", common.BLANKS, info.Type)
+				fmt.Printf("%sSubscribers: %d\n", common.BLANKS, len(info.Push.Subscribers))
+				fmt.Printf("%sRetries: %d\n", common.BLANKS, info.Push.Retries)
+				fmt.Printf("%sRetries delay: %d\n", common.BLANKS, info.Push.RetriesDelay)
+
+				if mqInfo.subscriberList {
+					fmt.Printf("%sSubscriber list\n", common.LINES)
+					common.PrintSubscribers(info)
+					fmt.Println()
+				}
+			}
+			common.PrintQueueHudURL(common.BLANKS, q)
+
+			return nil
 		},
 	}
 
--- cmd/mq/mq_list.go
+++ cmd/mq/mq_list.go
@@ -4,24 +4,68 @@ import (
 	"fmt"
 
 	"github.com/iron-io/iron_go3/config"
+	"github.com/iron-io/iron_go3/mq"
+	"github.com/iron-io/ironcli/common"
 	"github.com/urfave/cli"
 )
 
 type MqList struct {
+	page    string
+	perPage int
+	filter  string
+
 	cli.Command
 }
 
 func NewMqList(settings *config.Settings) *MqList {
-	mqList := &MqList{
-		Command: cli.Command{
-			Name:      "list",
-			Usage:     "do the doo",
-			UsageText: "doo - does the dooing",
-			ArgsUsage: "[image] [args]",
-			Action: func(c *cli.Context) error {
-				fmt.Println("added task: test ", c.Args().First())
-				return nil
+	mqList := &MqList{}
+	mqList.Command = cli.Command{
+		Name:      "list",
+		Usage:     "list of queues",
+		ArgsUsage: "[args]",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:        "page",
+				Usage:       "page usage",
+				Destination: &mqList.page,
 			},
+			cli.IntFlag{
+				Name:        "perPage",
+				Usage:       "perPage usage",
+				Destination: &mqList.perPage,
+			},
+			cli.StringFlag{
+				Name:        "filter",
+				Usage:       "filter usage",
+				Destination: &mqList.filter,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			queues, err := mq.FilterPage(mqList.filter, mqList.page, mqList.perPage)
+			if err != nil {
+				return err
+			}
+
+			if common.IsPipedOut() {
+				for _, q := range queues {
+					fmt.Println(q.Name)
+				}
+			} else {
+				fmt.Println(common.LINES, "Listing queues")
+				for _, q := range queues {
+					fmt.Println(common.BLANKS, "*", q.Name)
+				}
+
+				if tag, err := common.GetHudTag(*settings); err == nil {
+					fmt.Printf("%s Go to hud-e.iron.io/mq/%s/projects/%s/queues for more info",
+						common.BLANKS,
+						tag,
+						settings.ProjectId)
+				}
+				fmt.Println()
+			}
+
+			return nil
 		},
 	}
 
