fa8d3bdfd021088fd2434a6762e9629946df485d
Nyarum <puri.world@yandex.ru>
2016-08-30 23:13:09 +0400

    Finish lambda commands

--- cmd/lambda/lambda_publish_function.go
+++ cmd/lambda/lambda_publish_function.go
@@ -2,25 +2,58 @@ package lambda
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/iron-io/ironcli/common"
+	"github.com/iron-io/lambda/lambda"
 	"github.com/urfave/cli"
 )
 
 type LambdaPublishFunction struct {
+	functionName string
+
 	cli.Command
 }
 
 func NewLambdaPublishFunction() *LambdaPublishFunction {
-	lambdaPublishFunction := &LambdaPublishFunction{
-		Command: cli.Command{
-			Name:      "publish-function",
-			Usage:     "do the doo",
-			UsageText: "doo - does the dooing",
-			ArgsUsage: "[image] [args]",
-			Action: func(c *cli.Context) error {
-				fmt.Println("added task: test ", c.Args().First())
-				return nil
+	lambdaPublishFunction := &LambdaPublishFunction{}
+
+	lambdaPublishFunction.Command = cli.Command{
+		Name:  "publish-function",
+		Usage: "publish function for lambda",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:        "function-name",
+				Usage:       "function-name usage",
+				Destination: &lambdaPublishFunction.functionName,
 			},
+		},
+		ArgsUsage: "[NAME] [args]",
+		Action: func(c *cli.Context) error {
+			exists, err := lambda.ImageExists(lambdaPublishFunction.functionName)
+			if err != nil {
+				return err
+			}
+
+			if !exists {
+				return fmt.Errorf("Function %s does not exist:", lambdaPublishFunction.functionName)
+			}
+
+			err = lambda.PushImage(lambda.PushImageOptions{
+				NameVersion:   lambdaPublishFunction.functionName,
+				OutputStream:  common.NewDockerJsonWriter(os.Stdout),
+				RawJSONStream: true,
+			})
+			if err != nil {
+				return err
+			}
+
+			err = lambda.RegisterWithIron(lambdaPublishFunction.functionName)
+			if err != nil {
+				return err
+			}
+
+			return nil
 		},
 	}
 
--- cmd/lambda/lambda_test_function.go
+++ cmd/lambda/lambda_test_function.go
@@ -3,24 +3,64 @@ package lambda
 import (
 	"fmt"
 
+	"github.com/iron-io/lambda/lambda"
 	"github.com/urfave/cli"
 )
 
 type LambdaTestFunction struct {
+	functionName  string
+	clientContext string
+	payload       string
+
 	cli.Command
 }
 
 func NewLambdaTestFunction() *LambdaTestFunction {
-	lambdaTestFunction := &LambdaTestFunction{
-		Command: cli.Command{
-			Name:      "test-function",
-			Usage:     "do the doo",
-			UsageText: "doo - does the dooing",
-			ArgsUsage: "[image] [args]",
-			Action: func(c *cli.Context) error {
-				fmt.Println("added task: test ", c.Args().First())
-				return nil
+	lambdaTestFunction := &LambdaTestFunction{}
+
+	lambdaTestFunction.Command = cli.Command{
+		Name:      "test-function",
+		Usage:     "test function for lambda",
+		ArgsUsage: "[image] [args]",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:        "function-name",
+				Usage:       "function-name usage",
+				Destination: &lambdaTestFunction.functionName,
 			},
+			cli.StringFlag{
+				Name:        "client-context",
+				Usage:       "client-context usage",
+				Destination: &lambdaTestFunction.clientContext,
+			},
+			cli.StringFlag{
+				Name:        "payload",
+				Usage:       "payload usage",
+				Destination: &lambdaTestFunction.payload,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			exists, err := lambda.ImageExists(lambdaTestFunction.functionName)
+			if err != nil {
+				return err
+			}
+
+			if !exists {
+				return fmt.Errorf("Function %s does not exist.", lambdaTestFunction.functionName)
+			}
+
+			payload := ""
+			if lambdaTestFunction.payload != "" {
+				payload = lambdaTestFunction.payload
+			}
+
+			// Redirect output to stdout.
+			err = lambda.RunImageWithPayload(lambdaTestFunction.functionName, payload)
+			if err != nil {
+				return err
+			}
+
+			return nil
 		},
 	}
 
