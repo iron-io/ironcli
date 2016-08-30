9bf27e981ac48e589c54bce4052f1222cdd55103
Grigorij Eleskin <puri.world@yandex.ru>
2016-08-30 19:12:38 +0400

    Finish lambda_create_function command

--- cmd/functions/test.go
+++
@@ -1,9 +0,0 @@
-package functions
-
-import "github.com/spf13/cobra"
-
-var TestCmd = &cobra.Command{
-	Use: "test",
-	Run: func(cmd *cobra.Command, args []string) {
-	},
-}
--- cmd/lambda/lamda_create_function.go
+++ cmd/lambda/lamda_create_function.go
@@ -1,26 +1,96 @@
 package lambda
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"path/filepath"
 
+	"github.com/iron-io/ironcli/common"
+	"github.com/iron-io/lambda/lambda"
 	"github.com/urfave/cli"
 )
 
 type LambdaCreateFunction struct {
+	functionName string
+	runtime      string
+	handler      string
+	fileNames    []string
+
 	cli.Command
 }
 
 func NewLambdaCreateFunction() *LambdaCreateFunction {
-	lambdaCreateFunction := &LambdaCreateFunction{
-		Command: cli.Command{
-			Name:      "create-function",
-			Usage:     "do the doo",
-			UsageText: "doo - does the dooing",
-			ArgsUsage: "[image] [args]",
-			Action: func(c *cli.Context) error {
-				fmt.Println("added task: test ", c.Args().First())
-				return nil
+	lambdaCreateFunction := &LambdaCreateFunction{}
+
+	lambdaCreateFunction.Command = cli.Command{
+		Name:      "create-function",
+		Usage:     "create function for lambda",
+		ArgsUsage: "[file] [files, ...] [args]",
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:        "function-name",
+				Usage:       "function-name usage",
+				Destination: &lambdaCreateFunction.functionName,
 			},
+			cli.StringFlag{
+				Name:        "runtime",
+				Usage:       "runtime usage",
+				Destination: &lambdaCreateFunction.runtime,
+			},
+			cli.StringFlag{
+				Name:        "handler",
+				Usage:       "handler usage",
+				Destination: &lambdaCreateFunction.handler,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			lambdaCreateFunction.fileNames = append(lambdaCreateFunction.fileNames, c.Args().First())
+			lambdaCreateFunction.fileNames = append(lambdaCreateFunction.fileNames, c.Args().Tail()...)
+
+			files := make([]lambda.FileLike, 0, len(lambdaCreateFunction.fileNames))
+			opts := lambda.CreateImageOptions{
+				Name:          lambdaCreateFunction.functionName,
+				Base:          fmt.Sprintf("iron/lambda-%s", lambdaCreateFunction.runtime),
+				Package:       "",
+				Handler:       lambdaCreateFunction.handler,
+				OutputStream:  common.NewDockerJsonWriter(os.Stdout),
+				RawJSONStream: true,
+			}
+
+			if lambdaCreateFunction.handler == "" {
+				return errors.New("No handler specified.")
+			}
+
+			// For Java we allow only 1 file and it MUST be a JAR.
+			if lambdaCreateFunction.runtime == "java8" {
+				if len(lambdaCreateFunction.fileNames) != 1 {
+					return errors.New("Java Lambda functions can only include 1 file and it must be a JAR file.")
+				}
+
+				if filepath.Ext(lambdaCreateFunction.fileNames[0]) != ".jar" {
+					return errors.New("Java Lambda function package must be a JAR file.")
+				}
+
+				opts.Package = filepath.Base(lambdaCreateFunction.fileNames[0])
+			}
+
+			for _, fileName := range lambdaCreateFunction.fileNames {
+				file, err := os.Open(fileName)
+				defer file.Close()
+				if err != nil {
+					return err
+				}
+
+				files = append(files, file)
+			}
+
+			err := lambda.CreateImage(opts, files...)
+			if err != nil {
+				return err
+			}
+
+			return nil
 		},
 	}
 
